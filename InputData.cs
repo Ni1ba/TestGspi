7dac36537076de6c433feffe4157ea02af80ba11
Ni1ba <danila.baldin@mail.ru>
2024-06-28 19:26:31 +0300

    раскидал входные даннные и красиво

---
+++ InputData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGspi
+{
+    internal class InputData
+    {
+        
+        public List<Dictionary<string, string>> Task1() {
+
+            //входные данные для теста
+
+            List<Dictionary<string, string>> employees = new List<Dictionary<string, string>>();
+
+            Dictionary<string, string> Map = new Dictionary<string, string>();
+
+            Map["Имя"] = "Иван";
+            Map["Фамилия"] = "Иванов";
+            Map["Отдел"] = "Продажи";
+            Map["Зарплата"] = "50000";
+            employees.Add(Map);
+
+
+            Map = new();
+
+            Map["Имя"] = "Петр";
+            Map["Фамилия"] = "Петров";
+            Map["Отдел"] = "Маркетинг";
+            Map["Зарплата"] = "60000";
+            employees.Add(Map);
+
+            Map = new();
+
+            Map["Имя"] = "Анна";
+            Map["Фамилия"] = "Сидорова";
+            Map["Отдел"] = "Бухгалтерия";
+            Map["Зарплата"] = "45000";
+            employees.Add(Map);
+
+            Map = new();
+
+            Map["Имя"] = "Сергей";
+            Map["Фамилия"] = "Козлов";
+            Map["Отдел"] = "IT";
+            Map["Зарплата"] = "71000";
+            employees.Add(Map);
+
+            Map = new();
+
+            Map["Имя"] = "Елена";
+            Map["Фамилия"] = "Кикитина";
+            Map["Отдел"] = "Продажи";
+            Map["Зарплата"] = "55000";
+            employees.Add(Map);
+
+
+            Map = new();
+
+            Map["Имя"] = "Алексей";
+            Map["Фамилия"] = "Соколов";
+            Map["Отдел"] = "IT";
+            Map["Зарплата"] = "75000";
+            employees.Add(Map);
+
+            return employees;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,68 +7,18 @@ public class Program
     {
 
         //входные данные для теста
-
-        List<Dictionary<string, string>> employees = new List<Dictionary<string, string>>();
-
-        Dictionary<string, string> Map = new Dictionary<string, string>();
-
-        Map["Имя"] = "Иван";
-        Map["Фамилия"] = "Иванов";
-        Map["Отдел"] = "Продажи";
-        Map["Зарплата"] = "50000";
-        employees.Add(Map);
-
-
-        Map = new();
-
-        Map["Имя"] = "Петр";
-        Map["Фамилия"] = "Петров";
-        Map["Отдел"] = "Маркетинг";
-        Map["Зарплата"] = "60000";
-        employees.Add(Map);
-
-        Map = new();
-
-        Map["Имя"] = "Анна";
-        Map["Фамилия"] = "Сидорова";
-        Map["Отдел"] = "Бухгалтерия";
-        Map["Зарплата"] = "45000";
-        employees.Add(Map);
-
-        Map = new();
-
-        Map["Имя"] = "Сергей";
-        Map["Фамилия"] = "Козлов";
-        Map["Отдел"] = "IT";
-        Map["Зарплата"] = "71000";
-        employees.Add(Map);
-
-        Map = new();
-
-        Map["Имя"] = "Елена";
-        Map["Фамилия"] = "Кикитина";
-        Map["Отдел"] = "Продажи";
-        Map["Зарплата"] = "55000";
-        employees.Add(Map);
-
-       
-        Map=new();
-
-        Map["Имя"] = "Алексей";
-        Map["Фамилия"] = "Соколов";
-        Map["Отдел"] = "IT";
-        Map["Зарплата"] = "75000";
-        employees.Add(Map);
-
+        InputData inputData = new InputData();
+        List<Dictionary<string, string>> listTask1 = inputData.Task1();
 
 
         //Метод задания 1
         SimpleConverterToPerson converterToPerson = new();
-        List <Person> filtredList = converterToPerson.FilterEmployees(employees);
-
-
+        List <Person> filtredList = converterToPerson.FilterEmployees(listTask1);
         //вывод данных
         converterToPerson.PrintList(filtredList);
+
+
+
 
 
     }
